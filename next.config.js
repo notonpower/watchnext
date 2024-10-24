9f4108c780c9742da2dcdff1d53af125703ba6ca
Noton450 <79391702+Noton450@users.noreply.github.com>
2024-10-24 19:04:48 +0900

    Replace next.config.ts with next.config.js

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    output: 'export',
+    basePath: '/watchnext',
+    images: {
+      unoptimized: true,
+    },
+  }
+  
+  module.exports = nextConfig
