48cc4326245ae0c248388c9bc05b898a93fc52e9
立野雄大 <sasakitarorange@gmail.com>
2024-10-25 09:19:02 +0900

    Update platform.ts

--- src/utils/platform.ts
+++ src/utils/platform.ts
@@ -2,19 +2,23 @@ import { Platform } from '@/types';
 
 export function getPlatformLogo(platform: Platform, type: 'square' | 'full') {
   if (platform.name === 'Netflix') {
-    return `/images/netflix_${type}.webp`;
+    return type === 'full' ? '/images/netflix_full.webp' : '/images/netflix_square.webp';
   }
   if (platform.name === 'Prime Video') {
-    return `/images/primevideo_${type}.webp`;
+    return type === 'full' ? '/images/primevideo_full.webp' : '/images/primevideo_square.webp';
   }
   if (platform.name === 'U-NEXT') {
-    return `/images/unext_${type}.webp`;
+    return type === 'full' ? '/images/unext_full.webp' : '/images/unext_square.webp';
   }
   return '';
 }
 
 export function getImagePath(image: string) {
-  return image;
+  if (!image) return '';
+  if (image.startsWith('/images/')) {
+    return image;
+  }
+  return `/images${image}`;
 }
 
 export function createShortId(title: string) {
